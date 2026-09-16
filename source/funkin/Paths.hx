73adc9df7fb93829b7b6834bab291aaae9f699c8
GooberManGoober <123832142+GooberManGoober@users.noreply.github.com>
2026-09-16 16:18:06 -0400

    what the fuck

--- source/funkin/Paths.hx
+++ source/funkin/Paths.hx
@@ -147,29 +147,10 @@ class Paths
 		return getPath('images/$key', parentFolder, checkMods);
 	}
 
-	public static inline function obj(key:String, ?parentFolder:String, checkMods:Bool = true):String
+	public static inline function model(key:String, ?ext:String, checkMods:Bool = true):String
 	{
-		return getPath('models/$key.obj', parentFolder, checkMods);
-	}
-
-	public static inline function dae(key:String, ?parentFolder:String, checkMods:Bool = true):String
-	{
-		return getPath('models/$key.dae', parentFolder, checkMods);
-	}
-
-	public static inline function md2(key:String, ?parentFolder:String, checkMods:Bool = true):String
-	{
-		return getPath('models/$key.md2', parentFolder, checkMods);
-	}
-
-	public static inline function md5(key:String, ?parentFolder:String, checkMods:Bool = true):String
-	{
-		return getPath('models/$key.md5', parentFolder, checkMods);
-	}
-
-	public static inline function awd(key:String, ?parentFolder:String, checkMods:Bool = true):String
-	{
-		return getPath('models/$key.awd', parentFolder, checkMods);
+		final exts = ext != null ? [ext, 'gltf', 'glb', 'json', 'mtl', 'obj'] : ['gltf', 'glb', 'json', 'mtl', 'obj'];
+		return findFileWithExts('models/$key', exts, null, checkMods);
 	}
 	
 	/**
