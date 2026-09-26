bc291f98d0d7233657ca677b180123e5e9c42a5d
GooberManGoober <123832142+GooberManGoober@users.noreply.github.com>
2026-09-26 18:29:34 -0400

    Revert "removed maxwell :("
    This reverts commit 1a4d92ac170ce5463955da4ae44a2914eb88aa4a.

--- source/funkin/states/editors/MasterEditorMenu.hx
+++ source/funkin/states/editors/MasterEditorMenu.hx
@@ -24,6 +24,9 @@ class MasterEditorMenu extends MusicBeatState
 		'Week Editor',
 		'Menu Character Editor',
 		'New Chart Editor (Unfinished)',
+		#if FOXLITE_ALLOWED
+		'Maxwell',
+		#end
 	];
 	private var grpTexts:FlxTypedGroup<Alphabet>;
 	private var directories:Array<String> = [null];
@@ -142,6 +145,10 @@ class MasterEditorMenu extends MusicBeatState
 					FlxG.switchState(() -> new MenuCharacterEditorState());
 				case "New Chart Editor (Unfinished)":
 					FlxG.switchState(ChartEditorState.new);
+				#if FOXLITE_ALLOWED
+				case "Maxwell":
+					FlxG.switchState(MaxwellState.new);
+				#end
 			}
 			if (muteMusic)
 			{
